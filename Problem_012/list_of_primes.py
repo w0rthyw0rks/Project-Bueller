923d36ca97c511fd8b387f87f7941efb97700d9d
Kyle Langworthy <kyle@worthy.works>
2019-02-26 21:16:22 -0500

    Create list_of_primes.py

---
+++ Problem_012/list_of_primes.py
@@ -0,0 +1,45 @@
+# a "table" of primes up to 65500 is required to further optimize this solution
+# I'm going to make a list of primes and see if that will work
+
+import time
+import math
+
+start_time = time.time()
+
+def isPrime(n):
+    if n == 1:
+        return False
+    if n < 4:
+        return True
+    if n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
+    else:
+        r = math.sqrt(n)
+        f = 5
+        while f <= r:
+            if n % f == 0:
+                return False
+            if n % (f+2) == 0:
+                return False
+            f = f + 6
+        return True
+
+
+primes = []
+count = 1
+
+for i in range (1, 65500):
+    i = i + 2
+    if isPrime(i):
+        count = count + 1
+        primes.append(i)
+
+print(primes)
+print(len(primes))
+print(primes[-1])
+
+print("--- %s seconds ---" % (time.time() - start_time))
