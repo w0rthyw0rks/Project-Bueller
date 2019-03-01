6704b779426acc0f337939358a592bcd6f084312
Kyle Langworthy <kyle@worthy.works>
2019-03-01 13:27:32 -0500

    Create problem_014_v2.py

---
+++ problem_014/problem_014_v2.py
@@ -0,0 +1,28 @@
+# runs in 73 seconds
+# runs in 53 seconds
+# runs in 32 seconds
+# runs in 28 seconds
+# runs in 20 seconds
+
+import time
+start_time = time.time()
+
+def countChain(n):
+    if n == 1:
+        return 1
+    if n % 2 == 0:
+        return 1 + countChain(n / 2)
+    else:
+        return 2 + countChain((3 * n + 1) / 2)
+
+longest_chain = 0
+answer = 0
+
+for i in range(500000, 10**6):
+    if countChain(i) > longest_chain:
+        longest_chain = countChain(i)
+        answer = i
+
+print(answer)
+
+print("--- %s seconds ---" % (time.time() - start_time))
