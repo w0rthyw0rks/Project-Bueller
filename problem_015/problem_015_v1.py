451f2e40b9502fdb4a73e4dbf0154c946f181347
Kyle Langworthy <kyle@worthy.works>
2019-03-01 23:00:51 -0500

    Update problem_015_v1.py

--- problem_015/problem_015_v1.py
+++ problem_015/problem_015_v1.py
@@ -2,3 +2,15 @@
 # https://en.wikipedia.org/wiki/Binomial_coefficient
 # https://en.wikipedia.org/wiki/Factorial
 
+import math
+import time
+start_time = time.time()
+
+n = 20
+k = 20
+
+x = n + k
+
+print(math.factorial(x) / (math.factorial(n)*math.factorial(x - n)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
